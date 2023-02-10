4733acab9f398f7b5827dd4291de070ca9ec4d81
Joao Victor <jvpereirarocha@gmail.com>
2023-02-10 19:04:07 -0300

    updating the run file

--- manage.py
+++ manage.py
@@ -4,4 +4,6 @@ from os import getenv
 if __name__ == "__main__":
     environment = getenv("ENVIRONMENT") or "production"
     app = create_app(environment=environment)
-    app.run(host="127.0.0.1", port=5050, debug=True)
+    run_as_debug = app.config.get("DEBUG", False)
+    port_to_run = app.config.get("PORT")
+    app.run(host="127.0.0.1", port=port_to_run, debug=run_as_debug)
